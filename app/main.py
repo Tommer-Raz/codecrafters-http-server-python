a35f36c81b5b1147ac9c0f5e6c08af3385012f22
Tommer Raz <tommer.raz@gmail.com>
2025-04-18 12:29:39 +0300

    added request

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ def main():
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     # server_socket.accept() # wait for client
+    server_socket.listen(b"GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n")
     server_socket.accept()[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
