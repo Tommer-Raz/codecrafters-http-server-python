1d4d6b2eb452a9e31a46ec33fd3eb03b870466cd
Tommer Raz <tommer.raz@gmail.com>
2025-04-21 10:16:37 +0300

    added threads

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import socket  # noqa: F401
-
+import threading
 def send_res(conn, content):
     response = (
                     f"HTTP/1.1 200 OK\r\n"
@@ -9,6 +9,21 @@ def send_res(conn, content):
                     ).encode() + content.encode()
     conn[0].sendall(response)
 
+def handle_request(conn):
+    req = conn[0].recv(1024).decode()
+    endpoint = req.split(" ")[1]
+    if endpoint == "/":
+        conn[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    elif endpoint.startswith("/echo/"):
+        content = endpoint.removeprefix("/echo/")
+        send_res(conn, content)
+    elif endpoint == "/user-agent":
+        user_agant = req.split("\r\n")[2].removeprefix("User-Agent: ")
+        send_res(conn, user_agant)
+    else:
+        conn[0].sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
@@ -16,21 +31,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn = server_socket.accept() # wait for client
+    
     while True:
-        req = conn[0].recv(1024).decode()
-        endpoint = req.split(" ")[1]
-        if endpoint == "/":
-            conn[0].sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-        elif endpoint.startswith("/echo/"):
-            content = endpoint.removeprefix("/echo/")
-            send_res(conn, content)
-        elif endpoint == "/user-agent":
-            user_agant = req.split("\r\n")[2].removeprefix("User-Agent: ")
-            send_res(conn, user_agant)
-        else:
-            conn[0].sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-
+        conn = server_socket.accept() # wait for client
+        threading.Thread(target=handle_request, args=conn).start()
 
 if __name__ == "__main__":
     main()
