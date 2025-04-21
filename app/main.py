94474f1e5de6a80233989f78945ecbb32d328d29
Tommer Raz <tommer.raz@gmail.com>
2025-04-21 10:17:57 +0300

    added threads

--- app/main.py
+++ app/main.py
@@ -34,7 +34,7 @@ def main():
     
     while True:
         conn = server_socket.accept() # wait for client
-        threading.Thread(target=handle_request, args=conn).start()
+        threading.Thread(target=handle_request, args=(conn, )).start()
 
 if __name__ == "__main__":
     main()
