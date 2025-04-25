d4ee201d6fa7194fd99794fc7e2a71b21644d28c
Tommer Raz <tommer.raz@gmail.com>
2025-04-25 10:38:04 +0300

    compress content

--- app/main.py
+++ app/main.py
@@ -17,8 +17,7 @@ def send_res(conn, content, content_type="text/plain", encoding=None):
        content = gzip.compress(content)
     response += f"\r\n"
     
-    response = response.encode() + content.decode()
-    print(content)
+    response += content
     conn[0].sendall(response)
 
 def handle_request(conn):
